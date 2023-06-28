6651b3ac66dff5556f3d34b27c07a639797eab53
prems1428 <premkumarsolomonr@gmail.com>
2023-06-28 09:45:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, catchError, from, fromEvent, fromEventPattern, interval, map, of, pipe, take, zip } from 'rxjs';
-import { custObservable } from './custObservable';
+import { SharedService } from './shared.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -8,52 +8,71 @@ import { custObservable } from './custObservable';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
 
     title = 'InputDec';
-    select:boolean;
-    a:any;
-    b:any;
+    todo : any;
+    getVal : any;
+    isDisable:boolean = false;
+    profileForm = new FormGroup({
+      name : new FormControl(''),
+      age : new FormControl(''),
+      contact :new FormControl('')
+    });
+    constructor(private service : SharedService){
+      
+     }
 
-  constructor(){
-      this.select = false;
-      this.a = [
-        {name:'Sam',
-        age : '12',
-        status : true
-      },
-      {name:'Kumar',
-        age : '34',
-        status : false
-      },
-      {name:'Arun',
-        age : '45',
-        status : true
-      }
-      ];
-  }
+     ngOnInit(): void {
+       this.submit();
+     }
+     onSubmit(){
 
-  unCheck() {
-    for (var i = 0; i < this.a.length; i++) {
-      this.a[i].status = this.select;
-    }
-    this.getCheckedName();
-  }
-  selectAll() {
-    this.select = this.a.every(function(item:any) {
-        return item.status == true;
+      let data = this.profileForm.value;
+      console.log(this.profileForm.value);
+      this.service.post(data).subscribe(data=>{
       })
-    this.getCheckedName();
-  }
- 
-  getCheckedName(){
-    this.b = [];
-    for (var i = 0; i < this.a.length; i++) {
-      if(this.a[i].status)
-      this.b.push(this.a[i]);
-    }
-    this.b = JSON.stringify(this.b);
-  }
+      this.submit();
+      this.profileForm.reset();
+     }
 
+     submit(){
+      this.service.get().subscribe(x=>{
+        this.todo = x;
+      })
+     }
+     
+     editData(id : number){
 
-      }
+      this.isDisable = true;
+       this.service.getId(id).subscribe(x=>{
+        let getEditVal :any=[];
+        getEditVal = x;
+        
+        this.profileForm.setValue({
+          name : getEditVal.name,
+          age : getEditVal.age,
+          contact : getEditVal.contact
+        })
+
+       })
+
+       
+     }
+     updateData(id:number){
+
+      this.isDisable=false;
+      let data = this.profileForm.value;
+      this.service.update(id,data).subscribe(x=>{
+        this.submit();
+      })
+      this.profileForm.reset();
+     }
+     deleteData(id :number){
+
+      this.service.delete(id).subscribe(x=>{
+        this.submit();
+      })
+     }
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
